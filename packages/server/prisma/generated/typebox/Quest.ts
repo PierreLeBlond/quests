8418d74834621f3b0dcef95212ae0ee76a287538
Lespingal <pierre.lespingal@gmail.com>
2023-09-25 15:28:57 +0200

    fix: Fix github workflow

---
+++ packages/server/prisma/generated/typebox/Quest.ts
@@ -0,0 +1,23 @@
+import { Type, Static } from "@sinclair/typebox";
+
+export const Quest = Type.Object({
+  id: Type.String(),
+  name: Type.String(),
+  archived: Type.Optional(Type.Boolean()),
+  user_id: Type.String(),
+  user: Type.Object({
+    id: Type.String(),
+    github_username: Type.String(),
+  }),
+  index: Type.Number(),
+  steps: Type.Array(
+    Type.Object({
+      id: Type.String(),
+      description: Type.String(),
+      done: Type.Optional(Type.Boolean()),
+      quest_id: Type.String(),
+    })
+  ),
+});
+
+export type QuestType = Static<typeof Quest>;
